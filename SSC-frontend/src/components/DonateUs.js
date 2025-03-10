81e73be068122e0fbfceae5346f0bf09b69555aa
Kapil Rahangdale <126713176+kapcode22@users.noreply.github.com>
2025-03-10 16:29:50 +0530

    Update DonateUs.js

--- SSC-frontend/src/components/DonateUs.js
+++ SSC-frontend/src/components/DonateUs.js
@@ -17,7 +17,7 @@ const DonateUs = () => {
     const headers = { "Content-type": "application/json" };
 
     try {
-      const response = await fetch("http://localhost:5000/api/create-checkout-session", {
+      const response = await fetch("https://ssc-final-backend.onrender.com/api/create-checkout-session", {
         method: "POST",
         headers: headers,
         body: JSON.stringify(body)
