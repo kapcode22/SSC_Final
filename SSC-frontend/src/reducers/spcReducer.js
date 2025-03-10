5c5452ea05a3e0305e46461144044672969476fb
Kapil Rahangdale <126713176+kapcode22@users.noreply.github.com>
2025-03-10 16:28:26 +0530

    Update spcReducer.js

--- SSC-frontend/src/reducers/spcReducer.js
+++ SSC-frontend/src/reducers/spcReducer.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchTeam = () => async (dispatch) => {
   dispatch({ type: 'FETCH_TEAM_REQUEST' });
   try {
-    const res = await axios.get('http://localhost:5000/api/postHolders/spc');
+    const res = await axios.get('https://ssc-final-backend.onrender.com/api/postHolders/spc');
     dispatch({ type: 'FETCH_TEAM_SUCCESS', payload: res.data });
   } catch (err) {
     dispatch({ type: 'FETCH_TEAM_FAILURE', payload: err.message });
@@ -13,7 +13,7 @@ export const fetchTeam = () => async (dispatch) => {
 export const fetchEvents = () => async (dispatch) => {
   dispatch({ type: 'FETCH_EVENTS_REQUEST' });
   try {
-    const res = await axios.get('http://localhost:5000/api/events/spc');
+    const res = await axios.get('https://ssc-final-backend.onrender.com/api/events/spc');
     dispatch({ type: 'FETCH_EVENTS_SUCCESS', payload: res.data });
   } catch (err) {
     dispatch({ type: 'FETCH_EVENTS_FAILURE', payload: err.message });
