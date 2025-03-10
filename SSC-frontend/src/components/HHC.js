09ebcce2f90d249cecc7dd3ee5e70bc2b1925ec6
Kapil Rahangdale <126713176+kapcode22@users.noreply.github.com>
2025-03-10 16:30:28 +0530

    Update HHC.js

--- SSC-frontend/src/components/HHC.js
+++ SSC-frontend/src/components/HHC.js
@@ -23,7 +23,7 @@ const Hhc = () => {
 
   const fetchCarouselImages = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/sliderImages/hhc');
+      const response = await axios.get('https://ssc-final-backend.onrender.com/api/sliderImages/hhc');
       setCarouselImages(response.data); // Assuming response.data is an array of image URLs
     } catch (error) {
       console.error('Error fetching carousel images:', error);
@@ -53,7 +53,7 @@ const Hhc = () => {
             {carouselImages && carouselImages.length > 0 ? (
               carouselImages.map((carouselimage, index) => (
                 <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-                  <img className={hstyles.carousel_img} src={`http://localhost:5000/${carouselimage.imageUrl.replace(/\\/g, '/')}`} alt="" />
+                  <img className={hstyles.carousel_img} src={`https://ssc-final-backend.onrender.com/${carouselimage.imageUrl.replace(/\\/g, '/')}`} alt="" />
                 </div>
               ))
             ) : (
@@ -121,7 +121,7 @@ const Hhc = () => {
                 </button>
               </div>
             </div>
-            <img className={hstyles.img_ani} src={`http://localhost:5000/${event.image.replace(/\\/g, '/')}`} alt="" />
+            <img className={hstyles.img_ani} src={`https://ssc-final-backend.onrender.com/${event.image.replace(/\\/g, '/')}`} alt="" />
             <div className={hstyles.date}></div>
           </div>
         ))}
@@ -133,7 +133,7 @@ const Hhc = () => {
           {team.map((member, index) => (
             <div className={hstyles.profile_card} key={index}>
               <div className={hstyles.img}>
-                <img src={`http://localhost:5000/${member.image.replace(/\\/g, '/')}`} alt="" />
+                <img src={`https://ssc-final-backend.onrender.com/${member.image.replace(/\\/g, '/')}`} alt="" />
               </div>
               <div className={hstyles.caption}>
                 <h3>{member.name}</h3>
